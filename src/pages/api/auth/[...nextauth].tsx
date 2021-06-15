1773c9ea4101844a7aa52ff1f3db4ad900a025ec
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-06-15 00:32:55 -0300

    Configurando login com google

---
+++ src/pages/api/auth/[...nextauth].tsx
@@ -0,0 +1,21 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  jwt: {
+    signingKey: process.env.JWTSIGNIN_KEY,
+  },
+  session: {
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  callbacks: {
+    // TODO: adicionar callback para adicionar usuário
+    // https://next-auth.js.org/configuration/options#callbacks
+  },
+});
